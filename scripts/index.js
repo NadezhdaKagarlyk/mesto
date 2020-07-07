39174c7b5e31ff79b4d7dade2ce9fdee372cb589
Nadezhda Kagarlyk <Nadezhdakag@yandex.ru>
2020-07-07 21:04:33 +0300

    Добавлен скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,26 @@
-const openPopupButton = document.querySelector('.profile__button_edit');
+const openPopupButton = document.querySelector('.profile__button-edit');
 const closePopupButton = document.querySelector('.popup__button-close');
 const popup = document.querySelector('.popup');
-
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_job');
+const profileName = document.querySelector('.profile__title');
+const profileInfo = document.querySelector('.profile__subtitle');
+const formElement = document.querySelector('.popup__form');
 
 function togglePopup() {
   popup.classList.toggle('popup_is-open');
 }
 
-openPopupButton.addEventListener('click', togglePopup);
+openPopupButton.addEventListener("click", togglePopup);
+
+closePopupButton.addEventListener("click", togglePopup);
 
 
-closePopupButton.addEventListener('click',togglePopup);
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileInfo.textContent = jobInput.value;
+    togglePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
